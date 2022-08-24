8f6d4937f161777d43289dc855d1bcee669b625f
bonveeee <bonochieng@gmail.com>
2022-08-24 08:17:46 +0300

    added intro , datatypes

--- intro.rb
+++ intro.rb
@@ -1,1 +1,38 @@
-#Bonaventure
+# ruby is dynamic object oriented responsible for server-side apllications(read & write , netwwork requests and Database connections)
+# just as JS is mainly for client-side web applications
+
+#When you write object-oriented code, you're normally looking to model 
+# concepts from the real world in your code. example can be Song
+
+#mainly use of simple engilish keywords with simple naming conventions to denote
+
+#ruby file end with .rb
+
+# running of Ruby file
+# ruby intro.rb
+
+#comments by symbol #
+
+
+
+#outputting options
+#puts print return keyword
+# we also have p & pp used for inspection of data
+
+# this line will print "This is the mornning Standup" as output.
+#puts "Hello World!";
+#print "This is the mornning Standup"
+
+#IRB (the interactive Ruby)
+#is a Ruby Shell, complete with command-line history, line editing capabilities, and job control.
+
+# simple ruby code
+ #methods defined by key word def followed by method name
+#         def add num 
+#             puts num 
+#             num 
+#         end
+
+#Class names begin with a capital letter; classes are constants, and all Ruby constants start with a capital letter.
+
+
