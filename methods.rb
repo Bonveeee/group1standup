9cfa1a068b14a85c16d3edb5655091ad6a663b44
Bernardngari <bernardngari.ke@gmail.com>
2022-08-24 08:11:46 +0300

    Update methods file

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 #Benard
+#Methods are expressions that return a value, equivalent to JavaScript functions.
+#2 types of methods : Instance methods and Class methods.
+#method name, by convention, must be in lower case.
+#methods have an implicit return, whatever is on the last line.
+
+#class Members
+		
+#	def greet(name)
+#		puts "Hi #{name}"
+#	end
+#end
+
+#member1  = Members.new
+#member2 = Members.new
+#puts member2
+#puts member1
+#puts member1.greet("Ben")
+
+
+
+##Implicit return and return keyword 
+
+#def greet_me
+#	"Using return keyword stopped execution at this line"
+#	"Hi Ben"
+#end
+
+#puts greet_me
